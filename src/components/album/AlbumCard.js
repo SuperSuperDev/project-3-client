b3dabf030c13a3b72fa33fc7e91fecfa5b348a37
Steven Saunders <steven@supersuperstore.com>
2021-05-31 17:15:38 +0100

    Fixed bug with AlbumList causing error when no leadArtist has been returned

--- src/components/album/AlbumCard.js
+++ src/components/album/AlbumCard.js
@@ -15,7 +15,7 @@ function AlbumCard({ _id, name, cover, artists, leadArtist }) {
             </div>
             <p className="subtitle">
               {artists &&
-                <span key={`card-${leadArtist._id}`}>{leadArtist.name}</span>
+                <span>{leadArtist?.name}</span>
               }
             </p>
           </div>
