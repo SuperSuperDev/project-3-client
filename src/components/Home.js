eaf792ff9b97ccd2471c85880964432be4dd057e
Steven Saunders <steven@supersuperstore.com>
2021-06-01 13:34:07 +0100

    small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <>
-      <section className="homepage hero is-fullheight-with-navbar">
+      <section className="homepage hero is-fullheight-with-navbar" style="background: ">
         <div className="hero-body">
           <Link to="/"className="container">
             <div className="title has-text-centered is-1">🎶</div>
             <h1 className="title is-1 has-text-centered has-text-white">
               Cloudify
             </h1>
+            <video autoplay muted loop id="myVideo">
+  <source src="http://nonovium.com/wp-content/uploads/sites/5/2021/06/Loop-62061.mp4" type="video/mp4" />
+</video>
+
           </Link>
         </div>
       </section>
