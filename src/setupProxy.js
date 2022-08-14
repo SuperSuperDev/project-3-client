367d39e1e66145b2c78c8f364b9c8619b1f34664
SuperSuperStore <steven@supersuperstore.com>
2022-08-15 00:12:50 +0100

    Update proxy url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
-import dotenv from 'dotenv'
-dotenv.config()
 
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(
       '/api',
-      { target: process.env.API_URL || 'http://localhost:4000/api' } //* Update this line if your API is accessible on a port other than 8000, eg 4000
+      { target: 'http://localhost:4000/api' } //* Update this line if your API is accessible on a port other than 8000, eg 4000
     )
   )
 }
