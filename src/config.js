81d81f2fe1d2cd705cf7efdac56a277298867b86
SuperSuperStore <steven@supersuperstore.com>
2022-08-14 23:55:25 +0100

    Update proxy url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // const devUrl = process.env.REACT_APP_PROD_URL
 // const prodUrl = process.env.REACT_APP_PROD_URL
 // export const baseUrl = process.env.NODE_ENV === 'production' ? prodUrl : devUrl
-import dotenv from 'dotenv'
+const dotenv = require('dotenv')
 dotenv.config()
 export const baseUrl = process.env.API_URL || 'http://localhost:4000/api'
