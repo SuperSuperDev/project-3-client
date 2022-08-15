30a70c41bc1f8cf90ed59149ecfef6c2cb4da6b0
SuperSuperStore <steven@supersuperstore.com>
2022-08-15 13:33:46 +0100

    fix: install dotenv

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 // const devUrl = process.env.REACT_APP_PROD_URL
 // const prodUrl = process.env.REACT_APP_PROD_URL
 // export const baseUrl = process.env.NODE_ENV === 'production' ? prodUrl : devUrl
-// const dotenv = require('dotenv')
-// dotenv.config()
+const dotenv = require('dotenv')
+dotenv.config()
 const apiUrl = process.env.API_URL
 console.log('apiUrl :', apiUrl)
 export const baseUrl = process.env.API_URL
